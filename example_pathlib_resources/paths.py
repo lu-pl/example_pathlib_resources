043f88a8d7f4b0a080036b7f4b29986d9f7cdb9c
Lukas Plank <lupl@tuta.io>
2024-10-15 10:45:09 +0200

    feat: add __file__ data_path

--- example_pathlib_resources/paths.py
+++ example_pathlib_resources/paths.py
@@ -4,3 +4,4 @@ from pathlib import Path
 
 data_path_1 = Path("./data")
 data_path_2 = files("example_pathlib_resources.data")
+data_path_3 = Path(__file__).parent / "./data"
--- tests/test_some_test.py
+++ tests/test_some_test.py
@@ -1,13 +1,15 @@
+import re
+
 import pytest
 
-from example_pathlib_resources.paths import data_path_1, data_path_2
+from example_pathlib_resources.paths import data_path_1, data_path_2, data_path_3
 from example_pathlib_resources.some_module import read_data
 
 
 @pytest.mark.xfail(raises=FileNotFoundError)
 def test_read_data():
     data = read_data(data_path_1)
-    assert data == "this is some data"
+    assert re.match("this is some data", data)
 
 
 @pytest.mark.xfail(raises=AssertionError)
@@ -18,9 +20,19 @@ def test_data_pathsexists():
 
 def test_read_data2():
     data = read_data(data_path_2)
-    assert data == "this is some data\n"
+    assert re.match("this is some data", data)
 
 
 def test_data_pathsexists2():
     f = data_path_2 / "some_data.txt"
+    assert f.exists()
+
+
+def test_read_data3():
+    data = read_data(data_path_3)
+    assert data == "this is some data\n"
+
+
+def test_data_pathsexists3():
+    f = data_path_3 / "some_data.txt"
     assert f.exists()
